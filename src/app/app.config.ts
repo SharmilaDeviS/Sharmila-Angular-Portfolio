86399acb444272b25dcc53c681bd9324ba2334e7
Sharmila Devi S <sharmiladevi5795@gmail.com>
2026-03-27 19:45:50 +0530

    commit all

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withHashLocation, withInMemoryScrolling, withRouterConfig } from '@angular/router'; // Add withInMemoryScrolling
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(
+      routes, 
+      withHashLocation(),
+      withInMemoryScrolling({ 
+        anchorScrolling: 'enabled', // This enables the #ID scrolling
+        scrollPositionRestoration: 'enabled' 
+      }),
+      withRouterConfig({
+        onSameUrlNavigation: 'reload' // Ensures it works if you click the link twice
+      })
+    )
   ]
 };
